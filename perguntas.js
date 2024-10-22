b63f6b91b8a8a467ac21015cb17ff5f7f4dd239c
alissonsouzaferreira <166514120+alissonsouzaferreira@users.noreply.github.com>
2024-10-22 09:05:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+'programação',
+'O que é HTML?',
+'HTML é uma linguagem de marcação utilizada na construção de páginas na Web'
+)
+criaCartao(
+'cultura pop',
+'qual o personagem mais forte de jujutsu kaisen',
+'Ryomen Sukuna'
+)
+criaCartao(
+'cultura pop',
+'qual o filme com a maior bilheteria da historia',
+'avatar'
+)
+criaCartao(
+'cultura pop',
+'quantas partes tem jojo',
+'9 partes'
+)
